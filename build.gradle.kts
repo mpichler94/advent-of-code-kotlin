150d86bdac7c0de6a27609c14dbf354b6bf3bd83
Marcel Pichler <marcel.pichler@outlook.at>
2023-12-01 19:54:34 +0100

    solve day 01 2023

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.20"
     application
+
+    //id("io.gitlab.arturborsch.detekt") version "1.23.0"
 }
 
 group = "at.pichler"
---
+++ src/main/kotlin/at/mpichler/aoc/solutions/year2023/Day01.kt
@@ -0,0 +1,93 @@
+package at.mpichler.aoc.solutions.year2023
+
+import at.mpichler.aoc.lib.Day
+import at.mpichler.aoc.lib.PartSolution
+
+open class Part1A : PartSolution() {
+    internal lateinit var texts: List<String>
+
+    override fun parseInput(text: String) {
+        texts = text.trim().split("\n")
+    }
+
+    override fun getExampleAnswer(): Int {
+        return 142
+    }
+
+    internal fun getNumber(nums: String) = ("" + nums.first() + nums.last()).toInt()
+
+    override fun compute(): Int {
+        return texts.sumOf { it.filter(Char::isDigit).let(this::getNumber) }
+    }
+}
+
+class Part1B : Part1A() {
+    private val digits = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9"
+    )
+
+    override fun getExampleInput(): String {
+        return """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent()
+    }
+
+    override fun getExampleAnswer(): Int {
+        return 281
+    }
+
+    override fun compute(): Int {
+        var sum = 0
+        for (text in texts) {
+            var nums = text
+            while (true) {
+                val changed = replaceNumbers(nums)
+                if (changed == nums) {
+                    nums = changed
+                    break
+                }
+                nums = changed
+            }
+            nums = nums.filter { it.isDigit() }
+
+            sum += getNumber(nums)
+        }
+
+        return sum
+    }
+
+    private fun replaceNumbers(text: String): String {
+        var nums = text
+        for (i in nums.indices) {
+            for ((k, v) in digits) {
+                if (nums.substring(i).startsWith(k)) {
+                    nums = nums.replaceFirst(k, v)
+                    return nums
+                }
+                if (nums.substring(0, nums.length - i).endsWith(k)) {
+                    nums = nums.replace(k, v)
+                    return nums
+                }
+            }
+        }
+        return text
+    }
+}
+
+fun main() {
+    Day(2023, 1, Part1A(), Part1B())
+}
